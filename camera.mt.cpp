985c9437a193836882bee91b0a7d9798712bb647
Youtaro Mine <youtaro.mine@gmail.com>
2016-07-18 08:52:25 +0900

    Temporary fixed version for stablility on raspberry pi
    ToDo:
      remover gloval variable of image data
      add exclusive control with mutex and atomic operation again

--- camera.mt.cpp
+++ camera.mt.cpp
@@ -11,250 +11,156 @@
 #include <libuvc/libuvc_config.h>
 #include "libuvc/include/libuvc/libuvc_internal.h"
 
-#include <thread>
-#include <atomic>
-#include <mutex>
-#include <list>
-#include <tuple>
 using namespace std;
-static atomic<bool> gIsRunning(true);
-static mutex gInputImageMtx;
-static list<tuple<IplImage*,IplImage*>> gInputImages;
 
 void smd_cmd(uvc_device_handle_t *devh);
 
-static void addInputImage(IplImage* l,IplImage* r)
-{
-   std::lock_guard<std::mutex> lock(gInputImageMtx);
-   if(gInputImages.size() < 1)
-   {
-       gInputImages.push_front(std::make_tuple(l,r));
-// will released by stereoProcess
-       return;
-   }
-  cvReleaseImage(&l);
-  cvReleaseImage(&r);  
-}
-static void stereoProcess(IplImage* l,IplImage* r)
-{
-  IplImage* depth = cvCreateImage(cvGetSize(l),8,1);
-  IplImage* dest = cvCreateImage(cvGetSize(l),8,3);
-  cvFindStereoCorrespondence( l, r, CV_DISPARITY_BIRCHFIELD, depth, 127,15, 3, 6, 8, 15 );  
-  cvCvtColor(depth,dest,CV_GRAY2BGR);
-  cvScale(dest,dest,255/100);
-  cvNamedWindow("Depth", CV_WINDOW_AUTOSIZE);
-  cvShowImage("Depth", dest);
+IplImage* cvImg= cvCreateImage(cvSize(640,480),8,3);;
+uvc_frame_t *bgr = uvc_allocate_frame(640 * 480 * 3);
+IplImage* leftim = cvCreateImage(cvSize(320,480),8,3);
+IplImage* rightim = cvCreateImage(cvSize(320,480),8,3);
+IplImage* tmp = NULL;
+IplImage* l = cvCreateImage(cvGetSize(leftim),8,1);
+IplImage* r = cvCreateImage(cvGetSize(leftim),8,1);
+IplImage* depth = cvCreateImage(cvGetSize(l),8,1);
+IplImage* dest = cvCreateImage(cvGetSize(l),8,3);
 
-  cvReleaseImage(&depth);
-  cvReleaseImage(&dest);
-  cvReleaseImage(&l);
-  cvReleaseImage(&r);  
-}
-
-
-static void stereoThreadBody()
-{
-  while(gIsRunning)
-  {
-    IplImage* l = nullptr;
-    IplImage* r = nullptr;
-
-    // get .. last left & right frame.
-    {
-      std::lock_guard<std::mutex> lock(gInputImageMtx);
-      if(gInputImages.empty() == false) {
-        auto front = gInputImages.front();
-        l = std::get<0>(front);
-        r = std::get<1>(front);
-        gInputImages.pop_front();
-      }
-    }
-    if(l != nullptr && r != nullptr)
-    {
-      stereoProcess(l,r);
-    }
-
-  }
-}
-
+bool drawFlag=false;
+bool cbFlag=false;
+  
 void cb(uvc_frame_t *frame, void *ptr)
 {
-  uvc_frame_t *bgr=NULL;
-  uvc_error_t ret=(uvc_error_t)0;
-  IplImage* cvImg=NULL;
-  IplImage *leftim = cvCreateImage(cvSize(320,480),8,3);
-  IplImage *rightim = cvCreateImage(cvSize(320,480),8,3);
-  IplImage *tmp = NULL;
-  IplImage* l = cvCreateImage(cvGetSize(leftim),8,1);
-  IplImage* r = cvCreateImage(cvGetSize(leftim),8,1);
 
-  printf("callback! length = %u, ptr = %d\n", (unsigned int)frame->data_bytes, ( long)ptr);
-  bgr = uvc_allocate_frame(frame->width * frame->height * 3);
-  if(!bgr){
-    printf("unable to allocate bgr frame!\n");
-    return;
-  }
-  ret = uvc_any2bgr(frame, bgr);
-  if(ret){
-    uvc_perror(ret, "uvc_any2bgr");
-    return;
-  }
+    uvc_error_t ret=(uvc_error_t)0;
 
-  cvImg = cvCreateImageHeader(
-			      cvSize(bgr->width, bgr->height),
-			      IPL_DEPTH_8U,
-			      3);
-
-  cvSetData(cvImg, bgr->data, bgr->width * 3);
-
-  tmp=cvCloneImage(cvImg);
-  cvSetImageROI(tmp,cvRect(0,0,320,480));
-  cvResize(tmp,leftim,CV_INTER_LINEAR);
-  cvSetImageROI(tmp,cvRect(320,0,320,480));
-  cvResize(tmp,rightim,CV_INTER_LINEAR);
-  cvReleaseImage(&tmp);
-
-  cvNamedWindow("Left", CV_WINDOW_AUTOSIZE);
-  cvShowImage("Left", leftim);
-  cvWaitKey(10);
-
-  cvNamedWindow("Right", CV_WINDOW_AUTOSIZE);
-  cvShowImage("Right", rightim);
-
-  cvCvtColor(leftim,l,CV_BGR2GRAY);
-  cvCvtColor(rightim,r,CV_BGR2GRAY);
+    cbFlag=true;
+//  printf("callback! length = %u, ptr = %d\n", (unsigned int)frame->data_bytes, (long)ptr);
   
-  addInputImage(l,r);
-  cvNamedWindow("Original", CV_WINDOW_AUTOSIZE);
-  cvShowImage("Original", cvImg);
+    if(drawFlag == true) return;
 
-//  cvWaitKey(10);
+    ret = uvc_any2bgr(frame, bgr);
+    if(ret)uvc_perror(ret, "uvc_any2bgr");
 
-
-  cvReleaseImageHeader(&cvImg);
-  cvReleaseImageHeader(&leftim);
-  cvReleaseImageHeader(&rightim);
-
-  uvc_free_frame(bgr);
 }
+
+
+void smd_cmd(uvc_device_handle_t *devh)
+{
+    char buf[2];
+    int res=0;
+    libusb_device_handle *dev_handle=NULL;
+
+    buf[0] = 0x76;
+    buf[1] = 0xc3;
+    dev_handle = devh->usb_devh;
+    
+    res = libusb_claim_interface(dev_handle, 0);
+    if(res != LIBUSB_SUCCESS) printf("claim 0 is failed!\n");
+    res = libusb_control_transfer(devh->usb_devh,0x21,0x01,0x0800,0x0600,(unsigned char *)buf,sizeof(buf),0);
+    printf("res = %d\n",res);
+    if(res == sizeof(buf))
+        printf("Control transfer Success!\n");
+    else 
+        printf("Control transfer Failed!\n");
+
+
+    buf[0] = 0x04;
+    buf[1] = 0x00;
+    res = libusb_control_transfer(devh->usb_devh,0x21,0x01,0xa00,0x600,(unsigned char *)buf,sizeof(buf),0);
+    if(res == sizeof(buf))
+        printf("Control transfer Success!\n");
+    else 
+        printf("Control transfer Failed!\n");
+}
+
 
 int main()
 {
-  uvc_context_t *ctx=NULL;
-  uvc_device_t *dev=NULL;
-  uvc_device_handle_t *devh=NULL;
-  uvc_stream_ctrl_t ctrl;
-  uvc_error_t res=(uvc_error_t)0;
-  uvc_error_t resAEMODE=(uvc_error_t)0;
-  uvc_error_t resEXP=(uvc_error_t)0;
+    uvc_context_t *ctx=NULL;
+    uvc_device_t *dev=NULL;
+    uvc_device_handle_t *devh=NULL;
+    uvc_stream_ctrl_t ctrl;
+    uvc_error_t res=(uvc_error_t)0;
 
+    res = uvc_init(&ctx, NULL);
+    if (res < 0){
+        uvc_perror(res, "uvc_init");
+        return res;
+    }
+    puts("UVC initialized");
 
-  unsigned int i,j;
+    res = uvc_find_device(ctx, &dev,0, 0, NULL);
+    if (res < 0) {
+        uvc_perror(res, "uvc_find_device"); /* no devices found */
+        return res;
+    } else {
+        puts("Device found");
+    }
 
-  res = uvc_init(&ctx, NULL);
-
-  if (res < 0) {
-    uvc_perror(res, "uvc_init");
-    return res;
-  }
-  puts("UVC initialized");
-
-
-  res = uvc_find_device(
-			ctx, &dev,
-			0, 0, NULL);
-  if (res < 0) {
-    uvc_perror(res, "uvc_find_device"); /* no devices found */
-  } else {
-    puts("Device found");
-    
-    
     res = uvc_open(dev, &devh);
     if (res < 0) {
-      uvc_perror(res, "uvc_open"); /* unable to open device */
+        uvc_perror(res, "uvc_open"); /* unable to open device */
+        return res;
     } else {
-      puts("Device opened");
-      uvc_print_diag(devh, stderr);
-      
-      /* Try to negotiate a 640x480 30 fps YUYV stream profile */
-      res = uvc_get_stream_ctrl_format_size(
-					    devh, &ctrl, /* result stored in ctrl */
-					    UVC_FRAME_FORMAT_YUYV, /* YUV 422, aka YUV 4:2:2. try _COMPRESSED */
-					    640, 480, 30 /* width, height, fps */
-					    );
-      /* Print out the result */
-      uvc_print_stream_ctrl(&ctrl, stderr);
-      
-      if(res < 0){
-	uvc_perror(res, "get_mode");
-      }else{
-	res = uvc_start_streaming(devh, &ctrl, cb, (void *)12345, 0);
-	std::thread thd(stereoThreadBody);
-	thd.detach();
-	smd_cmd(devh);
-	if(res < 0){
-	  uvc_perror(res, "start_streaming");
-	}else{
-	  puts("Streaming for 10 seconds...");
-	  resAEMODE = uvc_set_ae_mode(devh, 1);
-	  uvc_perror(resAEMODE, "set_ae_mode");
-
-	  while(1){
-	    scanf("%d",i);
-//	    usleep(500000);
-	  }
-	  uvc_stop_streaming(devh);
-	  puts("Done streaming.");
-          gIsRunning = false;
-	}
-	 
-	uvc_close(devh);
-	puts("Device closed");
-      }
-      uvc_unref_device(dev);
+        puts("Device opened");
+        uvc_print_diag(devh, stderr);
     }
-  }
-  uvc_exit(ctx);
-  puts("UVC exited");
-  
-  return 0;
-}
-  
-void smd_cmd(uvc_device_handle_t *devh)
-{
-  char buf[2];
-  int res=0;
 
-  libusb_device_handle *dev_handle=NULL;
-  
-  buf[0] = 0x76;
-  buf[1] = 0xc3;
-  dev_handle = devh->usb_devh;
-  res = libusb_claim_interface(dev_handle, 0);
-  if(res != LIBUSB_SUCCESS) printf("claim 0 is failed!\n");
-  res = libusb_control_transfer(devh->usb_devh,
-				0x21,
-				0x01,
-				0x0800,
-				0x0600,
-				(unsigned char *)buf,
-				sizeof(buf),
-				0);
-  printf("res = %d\n",res);
-  if(res == sizeof(buf))printf("Control transfer Success!\n");
-  else printf("Control transfer Failed!\n");
-  
-  buf[0] = 0x04;
-  buf[1] = 0x00;
-  res = libusb_control_transfer(devh->usb_devh,
-				0x21,
-				0x01,
-				0xa00,
-				0x600,
-				(unsigned char *)buf,
-				sizeof(buf),
-				0);
-  if(res == sizeof(buf))printf("Control transfer Success!\n");
-  else printf("Control transfer Failed!\n");
-}
+    res = uvc_get_stream_ctrl_format_size(devh, &ctrl,UVC_FRAME_FORMAT_YUYV,640, 480, 30);
+    uvc_print_stream_ctrl(&ctrl, stderr);
+      
+    if(res < 0){
+        uvc_perror(res, "get_mode");
+        return res;
+    }
+    
+    res = uvc_start_streaming(devh, &ctrl, cb, (void *)12345, 0);
+    if(res < 0){
+        uvc_perror(res, "start_streaming");
+        return res;
+    }
 
+    smd_cmd(devh);
+
+    while(1){
+       if(cbFlag == true){
+            drawFlag = true;
+
+            cvImg = cvCreateImageHeader(cvSize(bgr->width, bgr->height),IPL_DEPTH_8U,3);
+
+            cvSetData(cvImg, bgr->data, bgr->width * 3);
+            cvNamedWindow("Original", CV_WINDOW_AUTOSIZE);
+            cvShowImage("Original", cvImg);
+
+            tmp=cvCloneImage(cvImg);
+            cvSetImageROI(tmp,cvRect(0,0,320,480));
+            cvResize(tmp,leftim,CV_INTER_LINEAR);
+            cvSetImageROI(tmp,cvRect(320,0,320,480));
+            cvResize(tmp,rightim,CV_INTER_LINEAR);
+            cvReleaseImage(&tmp);
+
+            cvCvtColor(leftim,l,CV_BGR2GRAY);
+            cvCvtColor(rightim,r,CV_BGR2GRAY);
+            cvFindStereoCorrespondence( l, r, CV_DISPARITY_BIRCHFIELD, depth, 127, 15, 3, 6, 8, 15 );  
+            cvCvtColor(depth,dest,CV_GRAY2BGR);
+            cvScale(dest,dest,255/100);
+            cvNamedWindow("Depth", CV_WINDOW_AUTOSIZE);
+            cvShowImage("Depth", dest);            
+
+            cvReleaseImageHeader(&cvImg);
+            drawFlag = false;
+            cvWaitKey(1);
+            cbFlag = false;
+        }
+    }
+    
+    uvc_stop_streaming(devh);
+    puts("Done streaming.");
+    uvc_close(devh);
+    puts("Device closed");
+    uvc_unref_device(dev);
+    uvc_exit(ctx);
+    puts("UVC exited");
+
+    return 0;
+}
