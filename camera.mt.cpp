92faf0d9126c26bfa4f729a211e5a338979854d8
Youtaro Mine <youtaro.mine@gmail.com>
2016-07-28 09:17:32 +0900

    start extract depth features as separate function

--- camera.mt.cpp
+++ camera.mt.cpp
@@ -52,57 +52,9 @@ void cb(uvc_frame_t *frame, void *ptr)
 
 }
 
-
-void smd_cmd(uvc_device_handle_t *devh)
+int initCamera()
 {
-    char buf[2];
-    int res=0;
-    libusb_device_handle *dev_handle=NULL;
-
-    buf[0] = 0x76;
-    buf[1] = 0xc3;
-    dev_handle = devh->usb_devh;
-    
-    res = libusb_claim_interface(dev_handle, 0);
-    if(res != LIBUSB_SUCCESS) printf("claim 0 is failed!\n");
-    res = libusb_control_transfer(devh->usb_devh,0x21,0x01,0x0800,0x0600,(unsigned char *)buf,sizeof(buf),0);
-    printf("res = %d\n",res);
-    if(res == sizeof(buf))
-        printf("Control transfer Success!\n");
-    else 
-        printf("Control transfer Failed!\n");
-
-
-    buf[0] = 0x04;
-    buf[1] = 0x00;
-    res = libusb_control_transfer(devh->usb_devh,0x21,0x01,0xa00,0x600,(unsigned char *)buf,sizeof(buf),0);
-    if(res == sizeof(buf))
-        printf("Control transfer Success!\n");
-    else 
-        printf("Control transfer Failed!\n");
-}
-
-
-int main()
-{
-    uvc_context_t *ctx=NULL;
-    uvc_device_t *dev=NULL;
-    uvc_device_handle_t *devh=NULL;
-    uvc_stream_ctrl_t ctrl;
-    uvc_error_t res=(uvc_error_t)0;
-    CvMoments moments10;
-    CvMoments moments20;
-    CvMoments moments30;
-    CvPoint center10;
-    CvPoint center20;
-    CvPoint center30;
-    
-    double distance = 0;
-    double angle = 0;
-
-    int count;
-
-    res = uvc_init(&ctx, NULL);
+	    res = uvc_init(&ctx, NULL);
     if (res < 0){
         uvc_perror(res, "uvc_init");
         return res;
@@ -139,24 +91,40 @@ int main()
         uvc_perror(res, "start_streaming");
         return res;
     }
+}
 
-    smd_cmd(devh);
+void smd_cmd(uvc_device_handle_t *devh)
+{
+    char buf[2];
+    int res=0;
+    libusb_device_handle *dev_handle=NULL;
 
-    while(1){
-       if(cbFlag == true){
-            drawFlag = true;
-            
-            distance = 0;
-            angle = 0;
-            
-            cvImg = cvCreateImageHeader(cvSize(bgr->width, bgr->height),IPL_DEPTH_8U,3);
+    buf[0] = 0x76;
+    buf[1] = 0xc3;
+    dev_handle = devh->usb_devh;
+    
+    res = libusb_claim_interface(dev_handle, 0);
+    if(res != LIBUSB_SUCCESS) printf("claim 0 is failed!\n");
+    res = libusb_control_transfer(devh->usb_devh,0x21,0x01,0x0800,0x0600,(unsigned char *)buf,sizeof(buf),0);
+    printf("res = %d\n",res);
+    if(res == sizeof(buf))
+        printf("Control transfer Success!\n");
+    else 
+        printf("Control transfer Failed!\n");
 
-            cvSetData(cvImg, bgr->data, bgr->width * 3);
 
-#ifdef SHOW_WINDOW
-            cvNamedWindow("Original", CV_WINDOW_AUTOSIZE);
-            cvShowImage("Original", cvImg);
-#endif //SHOW_WINDOW
+    buf[0] = 0x04;
+    buf[1] = 0x00;
+    res = libusb_control_transfer(devh->usb_devh,0x21,0x01,0xa00,0x600,(unsigned char *)buf,sizeof(buf),0);
+    if(res == sizeof(buf))
+        printf("Control transfer Success!\n");
+    else 
+        printf("Control transfer Failed!\n");
+}
+
+int getDepth(
+)
+{
 
             tmp=cvCloneImage(cvImg);
             cvSetImageROI(tmp,cvRect(0,120,320,360));
@@ -180,6 +148,25 @@ int main()
             cvNamedWindow("Depth", CV_WINDOW_AUTOSIZE);
             cvShowImage("Depth", dest);
 #endif //SHOW_WINDOW
+	
+}
+
+
+int getPosition( // 名前ももうちょっと考えたい
+// -- 引数を考える
+// 画像のポインタ(depth画像)
+// 取得したいdepth
+// ディスプレイ表示の有無
+)
+{
+    CvMoments moments10;
+    CvMoments moments20;
+    CvMoments moments30;
+    CvPoint center10;
+    CvPoint center20;
+    CvPoint center30;
+    
+// 固定値で入れているものをパラメータ化する
             
             cvThreshold( depth, depth10, 10.0, 255.0, CV_THRESH_BINARY );
             cvThreshold( depth, depth20, 20.0, 255.0, CV_THRESH_BINARY );
@@ -198,47 +185,63 @@ int main()
                 distance = 90.0;
                 angle = atan((((center10.x - 5) - 160)/(160/45))/distance);
             }
-            
+
+// 20と30でちがうのはここだけ
+                angle = atan((((center20.x - 10) - 160)/(160/22.5))/distance);
+                angle = atan((((center30.x - 15) - 160)/(160/15))/distance);
+
+
+
 #ifdef SHOW_WINDOW
             cvNamedWindow("Depth10", CV_WINDOW_AUTOSIZE);
             cvShowImage("Depth10", dest);
 #endif //SHOW_WINDOW
+}
 
-            cvMoments(depth20, &moments20,0);
-            center20.x = moments20.m10/moments20.m00;
-            center20.y = moments20.m01/moments20.m00;
+int main()
+{
+    uvc_context_t *ctx=NULL;
+    uvc_device_t *dev=NULL;
+    uvc_device_handle_t *devh=NULL;
+    uvc_stream_ctrl_t ctrl;
+    uvc_error_t res=(uvc_error_t)0;
+
+    
+    double distance = 0;
+    double angle = 0;
+
+    int count;
+
+//-- cameraの初期化
+
+
+//-- 特殊モード突入
+    smd_cmd(devh);
+
+//-- メインループ
+    while(1){
+       if(cbFlag == true){
+            drawFlag = true;
             
-            cvCvtColor(depth20,dest,CV_GRAY2BGR);
-            count = cvCountNonZero(depth20);
-//            printf("depth20:%d \n",count);
-            if(count > COUNT_THR){
-                cvCircle( dest, center20, 4, CV_RGB(255,0,0), 2, 4, 0);
-                distance = 45.0;
-                angle = atan((((center20.x - 10) - 160)/(160/22.5))/distance);
-            }
+            distance = 0;
+            angle = 0;
             
+            cvImg = cvCreateImageHeader(cvSize(bgr->width, bgr->height),IPL_DEPTH_8U,3);
+
+            cvSetData(cvImg, bgr->data, bgr->width * 3);
+
 #ifdef SHOW_WINDOW
-            cvNamedWindow("Depth20", CV_WINDOW_AUTOSIZE);
-            cvShowImage("Depth20", dest);
+            cvNamedWindow("Original", CV_WINDOW_AUTOSIZE);
+            cvShowImage("Original", cvImg);
 #endif //SHOW_WINDOW
 
-            cvMoments(depth30, &moments30,0);
-            center30.x = moments30.m10/moments30.m00;
-            center30.y = moments30.m01/moments30.m00;
-            
-            cvCvtColor(depth30,dest,CV_GRAY2BGR);
-            count = cvCountNonZero(depth30);
-//            printf("depth30:%d \n",count);
-            if(count > COUNT_THR){
-                cvCircle( dest, center30, 4, CV_RGB(255,0,0), 2, 4, 0);
-                distance = 30.0;
-                angle = atan((((center30.x - 15) - 160)/(160/15))/distance);
-            }
-            
-#ifdef SHOW_WINDOW
-            cvNamedWindow("Depth30", CV_WINDOW_AUTOSIZE);
-            cvShowImage("Depth30", dest);
-#endif //SHOW_WINDOW
+//-- depth画像取得
+
+//-- depth10の処理 --
+
+//-- depth20の処理 --
+
+//-- depth30の処理 --
 
             printf("distance %f, angle %f \n", distance, angle*180/CV_PI);
                       
@@ -259,3 +262,4 @@ int main()
 
     return 0;
 }
+
